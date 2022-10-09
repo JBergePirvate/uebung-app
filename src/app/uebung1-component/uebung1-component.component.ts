68e3396ddcaa3cea82d94912a4b010ef309abf2e
JBergePirvate <bergejohannes@gmail.com>
2022-10-09 12:18:08 +0200

    uebung1_ende

---
+++ src/app/uebung1-component/uebung1-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uebung1-component',
+  templateUrl: './uebung1-component.component.html',
+  styleUrls: ['./uebung1-component.component.css']
+})
+export class Uebung1ComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
