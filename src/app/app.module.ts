99667d30a86ae78f004c1b1a79ff8ee567ffd606
JBergePirvate <bergejohannes@gmail.com>
2022-10-09 13:33:45 +0200

    uebung1_start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Uebung1ComponentComponent } from './uebung1-component/uebung1-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Uebung1ComponentComponent
   ],
   imports: [
     BrowserModule,
