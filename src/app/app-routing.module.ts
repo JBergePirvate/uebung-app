824a68b6d6bbda633ea21a04d8e2b6129c6c2aa2
JBergePirvate <bergejohannes@gmail.com>
2022-10-12 15:12:42 +0200

    wierd stuff happened

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponentComponent } from './list-component/list-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'personen', component: ListComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
