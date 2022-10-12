3e019993e6b4483453d11bb92eed7e7dfd412604
JBergePirvate <bergejohannes@gmail.com>
2022-10-12 15:16:19 +0200

    ausgangsdateien geändert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponentComponent } from './list-component/list-component.component';
 
-const routes: Routes = [
-  {path:'personen', component: ListComponentComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
